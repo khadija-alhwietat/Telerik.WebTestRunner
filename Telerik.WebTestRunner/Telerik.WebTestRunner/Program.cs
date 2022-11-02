d54b8c98f1d1fafe47753c541477aeda51a7c490
khadeejeh.alhweitat <khadeejeh.alhweitat@maf.ae>
2022-11-02 17:35:06 +0200

    intial

---
+++ Telerik.WebTestRunner/Telerik.WebTestRunner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.WebTestRunner.Cmd;
+
+
+namespace Telerik.WebTestRunner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CmdRunner CmdRunner = new CmdRunner();
+            var arg = new List<string> { "Run" }.ToArray();
+            CmdRunner.Main(arg);
+        }
+    }
+}
